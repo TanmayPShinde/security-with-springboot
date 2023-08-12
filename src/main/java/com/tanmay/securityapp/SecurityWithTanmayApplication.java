bf3193bb1b067d94199dab43cee7379c43075962
TanmayPShinde <tps.shinde@gmail.com>
2023-08-12 22:17:53 +0530

    created User model, controller and repository with security

---
+++ src/main/java/com/tanmay/securityapp/SecurityWithTanmayApplication.java
@@ -0,0 +1,13 @@
+package com.tanmay.securityapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityWithTanmayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityWithTanmayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tanmay/securityapp/SecurityWithTanmayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tanmay.securityapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityWithTanmayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
